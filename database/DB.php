da88175f587587222f17eb58432f80893372962f
Gabriel Ascurra <gabriel@waveapps.com.ar>
2020-06-18 15:49:00 -0300

    Se agregó lo relacionado a la base de datos

---
+++ database/DB.php
@@ -0,0 +1,22 @@
+<?php
+class DB {
+    private $conexion=null;
+    private $host="localhost";
+    private $usuario="root";
+    private $contrasena="";
+    private $dbName="Prueba";
+
+    public function __construct() {
+        $this->conexion = new PDO("mysql:dbname=".$dbName.";host=".$host, $this->usuario, $this->contrasena,array(
+            PDO::ATTR_PERSISTENT => true
+        ));
+        $this->conexion->exec("SET CHARACTER SET utf8");
+        $this->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    
+    public function getConexion() {
+        return $this->conexion;
+    }
+
+
+}
---
+++ database/Persistencia.php
@@ -0,0 +1,44 @@
+<?php 
+require_once 'DB.php';
+
+class Persistencia{
+    private $conexion;
+    private static $instance;
+
+    public function __construct(){
+        $db = new DB();
+        $this->conexion = $db->getConexion();
+    }
+
+    public static function getInstance(){
+        //Esto es para hacer que sea un singleton
+        if(! self::$instance instanceof self){
+            self::$instance = new self;
+        }
+        return self::$instance;
+    }
+
+    public function iniciarTransaccion(){
+        $this->conexion->beginTransaction();
+    }
+    public function confirmarTransaccion() {
+        $this->conexion->commit();
+    }
+    public function revertirTransaccion() {
+        $this->conexion->rollBack();
+    }
+
+    public function ejecutarSentencia($query, $parametros = null){
+        $statement = $this->conexion->prepare($query);
+        
+        if ($parametros) {
+            $index = 1;
+            foreach ($parametros as $key => $parametro) {
+                $statement->bindValue($index, $parametro);
+                $index++;
+            }
+        }
+        $statement->execute();
+        return $statement;
+    }
+}
